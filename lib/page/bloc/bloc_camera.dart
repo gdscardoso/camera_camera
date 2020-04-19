7a0b5a24ffec4b8b50857fb5fe5bd3d332c083e1
Guilherme Cardoso <guilherme.s.cardoso@gmailcom>
2020-04-19 18:58:07 -0300

    fix cell phone two camera

--- lib/page/bloc/bloc_camera.dart
+++ lib/page/bloc/bloc_camera.dart
@@ -72,9 +72,8 @@ class BlocCamera {
 
   void changeCamera() {
     var list = cameras.value;
-    if (list.length == 2) {
-      if (controllCamera.description.lensDirection ==
-          CameraLensDirection.back) {
+    if (list.length >= 2) {
+      if (controllCamera.description.lensDirection == CameraLensDirection.back) {
         onNewCameraSelected(list[1]);
         cameraOn.sink.add(1);
       } else {
